4407482787acfbff6281c34fd13527d61f60d52d
artem <artemoon2008@gmail.com>
2022-06-18 12:17:50 +0300

    second

---
+++ js/script.js
@@ -0,0 +1,80 @@
+
+//*********006 КЛАССИФИКАЦИЯ ТИПОВ ДАННЫХ В JAVASCRIPT
+
+// let number = 4.6;
+
+// console.log(4/0);
+
+// const persone = '5';
+
+// const bool = false;
+
+// //console.log(something);
+
+// let und;
+// console.log(und);
+
+// const obj = {
+//     name: 'Jone',
+//     age: 25, 
+//     isMarried: false
+// };
+
+// console.log(obj.name);
+
+
+//  let arr = ['plum.png', 'orange.jpg', 6, 'apple.bmp', {}, []];
+//  console.log(arr[4]);
+
+//*********007 ПРОСТОЕ ОБЩЕНИЕ С ПОЛЬЗОВАТЕЛЕМ 
+
+// alert('hello');
+
+// const result = confirm('are you here');
+// console.log(result);
+
+// const answer = +prompt('вам есть 18','18');
+// console.log(answer + 5);
+
+// const answer = [];
+
+// answer[0] = prompt('Как ваше имя?', '');
+// answer[1] = prompt('Как ваше фамилия?', '');
+// answer[2] = prompt('Сколько вам лет?', '');
+
+// console.log(typeof(answer));
+// console.log(typeof(null));
+
+//*********008 ИНТЕРПОЛЯЦИЯ (ES6)
+
+// const category = 'toys';
+
+// console.log(`https://someurl.com/${category}/5`);
+
+// const user = 'Ivan';
+
+// alert(`првиет, ${user}`);
+
+//**********009 ОПЕРАТОРЫ В JS 
+
+// console.log('arr' + " - object");
+
+//  let incr = 10,
+//     decr = 10;
+
+// // incr++;
+// // decr--;
+
+// console.log(++incr);
+// console.log(--decr);
+
+// console.log(5%2);
+
+// console.log(2 + 2 * 2 !== "6");
+
+// const isChecked = false,
+//         isClose = false;
+
+// console.log(isChecked || !isClose);
+
+//**********010 УЧИМСЯ РАБОТАТЬ С СИСТЕМОЙ КОНТРОЛЯ GIT И С СЕРВИСОМ GitHub
