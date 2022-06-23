47fc035c70c62d95c7621df73da65e87c6591ffa
artem <artemoon2008@gmail.com>
2022-06-23 14:49:29 +0300

    fix structure

---
+++ js/learn_12.js
@@ -0,0 +1,19 @@
+const number0fFilms = prompt('Сколько фильмов вы уже посмотрели?');
+
+const personalMovieDB = {
+    count: number0fFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const a = prompt('Один из последних просмотренных фильмов?');
+const b = prompt('На сколько оцените его?');
+const c = prompt('Один из последних просмотренных фильмов?');
+const d = prompt('На сколько оцените его?');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(typeof (personalMovieDB.genres));
