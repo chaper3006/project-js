fd8e0cf665769496a7efa7cea148047821a98022
artem <artemoon2008@gmail.com>
2022-06-23 13:29:50 +0300

    films

--- js/script.js
+++ js/script.js
@@ -1,80 +1,20 @@
+const number0fFilms = prompt('Сколько фильмов вы уже посмотрели?');
 
-//*********006 КЛАССИФИКАЦИЯ ТИПОВ ДАННЫХ В JAVASCRIPT
+const personalMovieDB = {
+    count: number0fFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
 
-// let number = 4.6;
+const a = prompt('Один из последних просмотренных фильмов?');
+const b = prompt('На сколько оцените его?');
+const c = prompt('Один из последних просмотренных фильмов?');
+const d = prompt('На сколько оцените его?');
 
-// console.log(4/0);
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
 
-// const persone = '5';
+console.log(typeof (personalMovieDB.genres));
 
-// const bool = false;
-
-// //console.log(something);
-
-// let und;
-// console.log(und);
-
-// const obj = {
-//     name: 'Jone',
-//     age: 25, 
-//     isMarried: false
-// };
-
-// console.log(obj.name);
-
-
-//  let arr = ['plum.png', 'orange.jpg', 6, 'apple.bmp', {}, []];
-//  console.log(arr[4]);
-
-//*********007 ПРОСТОЕ ОБЩЕНИЕ С ПОЛЬЗОВАТЕЛЕМ 
-
-// alert('hello');
-
-// const result = confirm('are you here');
-// console.log(result);
-
-// const answer = +prompt('вам есть 18','18');
-// console.log(answer + 5);
-
-// const answer = [];
-
-// answer[0] = prompt('Как ваше имя?', '');
-// answer[1] = prompt('Как ваше фамилия?', '');
-// answer[2] = prompt('Сколько вам лет?', '');
-
-// console.log(typeof(answer));
-// console.log(typeof(null));
-
-//*********008 ИНТЕРПОЛЯЦИЯ (ES6)
-
-// const category = 'toys';
-
-// console.log(`https://someurl.com/${category}/5`);
-
-// const user = 'Ivan';
-
-// alert(`првиет, ${user}`);
-
-//**********009 ОПЕРАТОРЫ В JS 
-
-// console.log('arr' + " - object");
-
-//  let incr = 10,
-//     decr = 10;
-
-// // incr++;
-// // decr--;
-
-// console.log(++incr);
-// console.log(--decr);
-
-// console.log(5%2);
-
-// console.log(2 + 2 * 2 !== "6");
-
-// const isChecked = false,
-//         isClose = false;
-
-// console.log(isChecked || !isClose);
-
-//**********010 УЧИМСЯ РАБОТАТЬ С СИСТЕМОЙ КОНТРОЛЯ GIT И С СЕРВИСОМ GitHub
